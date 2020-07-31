1b38bb37a29197b7eea783efaf829645d8534f32
anya926 <68541162+anya926@users.noreply.github.com>
2020-07-31 10:25:39 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,25 @@
+class Box{
+    constructor(x,y,width,height){
+        var options = {
+            'restitution':0,
+            'friction':0.3,
+            'density':1.0,
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER);
+        stroke(255,0,0);
+        fill(255,0,0);
+        rect(0, 0, this.width, this.height);
+        pop();
+    }
+}
